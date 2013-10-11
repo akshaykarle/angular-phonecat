e7dc02dd9c45a32bd3efc9607d27d981147aed36
Akshay Karle <akshay.a.karle@gmail.com>
2013-10-11 18:02:58 +0530

    Update karma.conf.js

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -21,7 +21,7 @@ module.exports = function(config){
     customLaunchers: {
       Chrome_without_security: {
         base: 'Chrome',
-        flags: ['--disable-web-security']
+        flags: ['--no-sandbox']
       }
     },
 
